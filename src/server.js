3cea8b6b95ae292a873080a4753703b94dc923bf
Olesya Dragalova <olesya.sidyaka@gmail.com>
2016-08-11 00:00:13 +0300

    path to server fixed

--- src/server.js
+++ src/server.js
@@ -10,10 +10,10 @@ const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/../views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.get('/', function(request, response) {
 	console.time('render');
